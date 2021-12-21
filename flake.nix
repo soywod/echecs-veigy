bcdd28d59e34d60483e40a7670e3f3d42524a777
Clément DOUIN <clement.douin@posteo.net>
2021-12-21 16:25:14 +0100

    nix: init flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Website for the chess club of Veigy-Foncenex.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          name = (builtins.fromJSON (builtins.readFile ./package.json)).name;
+          pkgs = import nixpkgs { inherit system; };
+          next = "yarn run --offline --ignore-scripts --ignore-engines -- next";
+        in
+          rec {
+            # nix build
+            defaultPackage = pkgs.yarn2nix-moretea.mkYarnPackage {
+              inherit name;
+              src = ./.;
+              configurePhase = "ln -s $node_modules node_modules";
+              buildPhase = "${next} build";
+              installPhase = "${next} export -o $out";
+              distPhase = "exit";
+            };            
+
+            # nix develop
+            devShell = pkgs.mkShell {
+              inputsFrom = [ self.defaultPackage.${system} ];
+            };
+          }
+      );
+}
